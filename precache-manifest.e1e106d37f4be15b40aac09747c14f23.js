b9ab50fc100e380f650031eb85a23715d801f3ee
Armando <a.guerra58@hotmail.com>
2021-10-19 22:45:46 -0300

    Updates

---
+++ precache-manifest.e1e106d37f4be15b40aac09747c14f23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "305bca6a73f33c7f0d9ee065ca9c3152",
+    "url": "/wallet/index.html"
+  },
+  {
+    "revision": "79f90ad682536002b5e1",
+    "url": "/wallet/static/css/main.38ca0262.chunk.css"
+  },
+  {
+    "revision": "8d86b4c6950200266379",
+    "url": "/wallet/static/js/2.7ff40011.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/wallet/static/js/2.7ff40011.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79f90ad682536002b5e1",
+    "url": "/wallet/static/js/main.2c3c9568.chunk.js"
+  },
+  {
+    "revision": "32abb29ede4798555f19",
+    "url": "/wallet/static/js/runtime-main.86db26d2.js"
+  }
+]);
